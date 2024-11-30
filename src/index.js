6d2f3d8ef377fbff190d16a9198a7f96d0ab8f10
ThreePixDroid <threepixdroid@gmail.com>
2024-11-30 18:18:30 +0700

    moved the index file to the src folder

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { Core } from './core/Core'
+export { Curve } from './controllers/Curve'
+export { FromTo } from './controllers/FromTo'
+export { DEFAULTS } from './core/defaults'
+export { easings } from './behaviors/easings'
+export { EVENTS } from './core/events'
+export { modes } from './behaviors/modes'
+export { Manager } from './core/Manager'
